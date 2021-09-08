c38f07b6dca9737787a0cc26b209475a0d506058
Oleh <oleg3322110@gmail.com>
2021-09-08 14:00:46 +0300

    Add project files.

---
+++ Lab1/Lab1.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double S1, S2; //S1 -- area of the circumscribed square, S2 -- area of the inscribed square
+    cout << "S1 = "; cin >> S1; //initialization of S1
+    S2 = S1 / 2; //calculation of S2
+    printf("S2 = %0.3f\n", S2); //output of the result
+    cout << "S2 / S1 = 2"; //output of the result
+}
