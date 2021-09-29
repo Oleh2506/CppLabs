ec2a9a16b8b9ea5f880b5aa3340e505e3151d498
Oleh2506 <oleg3322110@gmail.com>
2021-09-29 12:25:12 +0300

    comments were corrected 2.0

--- Lab1/Lab1.cpp
+++ Lab1/Lab1.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main()
 {
     double S1, S2; //declaration of variables; S1 -- area of the circumscribed square, S2 -- area of the inscribed square
-    cout << "S1 = "; cin >> S1; //assignment of S1
+    cout << "S1 = "; cin >> S1; //input of variable
     S2 = S1 / 2; //calculation of S2
     printf("S2 = %f\n", S2); //output of the result
     cout << "S2 / S1 = 2"; //output of the result
