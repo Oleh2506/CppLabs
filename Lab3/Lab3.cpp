6c21a4a20d9e11931287b85857108287be79b56c
Oleh2506 <oleg3322110@gmail.com>
2021-10-12 18:26:01 +0300

    Lab3 was added

---
+++ Lab3/Lab3.cpp
@@ -0,0 +1,63 @@
+﻿// variant 2
+
+/*
+* Program to find natural logarithm using mathematical series
+*/
+
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+	float varA; //declaring a variable, the natural logarithm of which will be calculated
+
+	cout << "Input variable \"a\" in the range (0;2]: ";
+	cin >> varA; // data input
+
+	bool inputDataIsCorrect; //declaring boolean variable that describes the correctness of input data
+	if ((varA > 0) && (varA <= 2)) // checking the correctness of input data
+	{
+		inputDataIsCorrect = true;
+	}
+	else
+	{
+		inputDataIsCorrect = false;
+	}
+
+	if (inputDataIsCorrect)
+	{
+		float currentElem = varA - 1, LnA = 0, i = 0, percentError; //current element of the series, natural log of a, counter, percent error
+		const float CALCULATION_ACCURACY = 0.000001; // accuracy of calculation
+
+		cout << endl << "Elements of the series: " << endl;
+
+		// finding the natural logarithm value
+		do
+		{
+			cout << currentElem << " [" << i << "]; "; // element of the series output
+			LnA += currentElem;
+			i++;
+			currentElem *= ((-1) * (varA - 1) * i) / (i + 1);
+		} while (abs(currentElem) > CALCULATION_ACCURACY);
+
+		// percent error calculation
+		if (varA != 1) 
+		{
+			percentError = (abs(LnA - log(varA)) / log(varA)) * 100;
+		}
+		else
+		{
+			percentError = 0;
+		}
+
+		cout << endl << endl << "Ln(a): " << LnA << endl << "Percent error: " << percentError << " %" << endl; // result message output
+	}
+	else
+	{
+		cout << "Error: input data aren't correct!" << endl; // error message output
+	}
+
+	
+}
