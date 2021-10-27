45daf7f19bac6e1b548dbeec9f008e153e21eb49
Oleh2506 <oleg3322110@gmail.com>
2021-10-27 12:02:20 +0300

    Types of the variables were changed

--- Lab4/Lab4.cpp
+++ Lab4/Lab4.cpp
@@ -6,32 +6,32 @@
 #include <math.h>
 #include <iomanip>
 
-//Returns the Nth element of the series.  
-float findNthElement(unsigned short n);
+//Returns the Nth element of the series
+long double findNthElement(unsigned long long n);
 
 //Returns the sum of elements from 1 to nth
-float findSeriesSum(unsigned short n);
+long double findSeriesSum(unsigned long long n);
 
 int main()
 {
-    unsigned short n; //the number of elemnts, sum of which will be calculated
+    unsigned long long n; //the number of elemnts, sum of which will be calculated
     std::cout << "Please, input natural number N: ";
     std::cin >> n;
 
-    float seriesSum;
+    long double seriesSum;
     seriesSum = findSeriesSum(n);
+    std::cout << "Sum of the elements: " << std::fixed << std::setprecision(15) << seriesSum << "\n";
 
-    std::cout << "Sum of the elements: " << std::fixed << std::setprecision(10) << seriesSum << "\n";
     system("pause");
     return 0;
 }
 
 //Returns the sum of elements of the series from 1 to n
-float findSeriesSum(unsigned short n)
+long double findSeriesSum(unsigned long long n)
 {
-    float seriesSum(0);
-    float currentElem;
-    for (unsigned short i = 1; i <= n; ++i)
+    long double seriesSum(0);
+    long double currentElem;
+    for (unsigned long long i = 1; i <= n; ++i)
     {
         currentElem = findNthElement(i);
         seriesSum += currentElem;
@@ -39,10 +39,10 @@ float findSeriesSum(unsigned short n)
     return seriesSum;
 }
 
-//Returns the Nth element of the series.  
-float findNthElement(unsigned short n)
+//Returns the Nth element of the series
+long double findNthElement(unsigned long long n)
 {
-    float NthElement;
+    long double NthElement;
     NthElement = 1. / (n * pow(2 * n + 1, 2));
     return NthElement;
 }
