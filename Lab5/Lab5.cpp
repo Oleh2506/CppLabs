64535ce7861fb279b1f7ab491b1ce0d2f5aafc96
Oleh2506 <oleg3322110@gmail.com>
2021-11-02 20:48:06 +0200

    Lab5 was added

---
+++ Lab5/Lab5.cpp
@@ -0,0 +1,28 @@
+﻿/*
+* Variant 2. Program to complement the given number with two digits.
+* The required number has to be divisible by another specified number.
+*/
+
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+    const int INITIAL_NUM = 22200, DIVISOR = 15, FIRST_DIGIT_POS = 1, SECOND_DIGIT_POS = 0;
+    // code to find and output numbers satisfying given condition
+   
+    for (int i = 0; i <= 9; ++i)     // loop to find first undefined digit of the given number
+    {
+        for (int j = 0; j <= 9; ++j) // loop to find second undefined digit of the given number
+        {
+            int currentNum;
+            currentNum = INITIAL_NUM + i * pow(10, FIRST_DIGIT_POS) + j * pow(10, SECOND_DIGIT_POS);
+
+            if (currentNum % DIVISOR == 0)
+            {
+                std::cout << currentNum << " mod " << DIVISOR << " == 0\n";
+            }
+        }
+    }
+    return 0;
+}
