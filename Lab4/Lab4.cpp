1000c1d3067b45a5c53306a467458bdb98dfb6e0
Oleh2506 <oleg3322110@gmail.com>
2021-11-03 12:42:15 +0200

    Lab4: data checking was added

--- Lab4/Lab4.cpp
+++ Lab4/Lab4.cpp
@@ -7,31 +7,44 @@
 #include <iomanip>
 
 //Returns the Nth element of the series
-long double findNthElement(unsigned long long n);
+long double findNthElement(long long n);
 
 //Returns the sum of elements from 1 to nth
-long double findSeriesSum(unsigned long long n);
+long double findSeriesSum(long long n);
+
+//Returns true if the number is positive and false if not
+bool isPositiveNum(long long n);
 
 int main()
 {
-    unsigned long long n; //the number of elemnts, sum of which will be calculated
+    long long n; //the number of elemnts, sum of which will be calculated
     std::cout << "Please, input natural number N: ";
     std::cin >> n;
 
-    long double seriesSum;
-    seriesSum = findSeriesSum(n);
-    std::cout << "Sum of the elements: " << std::fixed << std::setprecision(15) << seriesSum << "\n";
+    bool isNaturalN;
+    isNaturalN = isPositiveNum(n);
+    if (isNaturalN)
+    {
+        long double seriesSum;
+        seriesSum = findSeriesSum(n);
+        // long double has a minimum precision of 15 significant digits
+        std::cout << "Sum of the elements: " << std::fixed << std::setprecision(15) << seriesSum << "\n";
+    }
+    else
+    {
+        std::cout << "Error: input number is not natural!\n";
+    }
 
     system("pause");
     return 0;
 }
 
 //Returns the sum of elements of the series from 1 to n
-long double findSeriesSum(unsigned long long n)
+long double findSeriesSum(long long n)
 {
     long double seriesSum(0);
     long double currentElem;
-    for (unsigned long long i = 1; i <= n; ++i)
+    for (long long i = 1; i <= n; ++i)
     {
         currentElem = findNthElement(i);
         seriesSum += currentElem;
@@ -40,9 +53,22 @@ long double findSeriesSum(unsigned long long n)
 }
 
 //Returns the Nth element of the series
-long double findNthElement(unsigned long long n)
+long double findNthElement(long long n)
 {
     long double NthElement;
     NthElement = 1. / (n * pow(2 * n + 1, 2));
     return NthElement;
+}
+
+//Returns true if the number is positive and false if not
+bool isPositiveNum(long long n)
+{
+    if (n > 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
--- Lab5/Lab5.cpp
+++ Lab5/Lab5.cpp
@@ -8,7 +8,7 @@
 
 int main()
 {
-    const int INITIAL_NUM = 22200, DIVISOR = 15, FIRST_DIGIT_POS = 1, SECOND_DIGIT_POS = 0;
+    const int INITIAL_NUM(22200), DIVISOR(15), FIRST_DIGIT_POS(1), SECOND_DIGIT_POS(0);
     // code to find and output numbers satisfying given condition
    
     for (int i = 0; i <= 9; ++i)     // loop to find first undefined digit of the given number
